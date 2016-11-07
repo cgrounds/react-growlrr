c288c772dd01e4001c3f79e5cd5588ec27738ab8
Bryony McLaughlin <bryonymc@gmail.com>
2016-11-07 15:02:25 -0600

    added delete button

--- src/Growl.js
+++ src/Growl.js
@@ -3,9 +3,13 @@ import React, {Component} from 'react';
 
 class Growl extends Component {
   render() {
+    let deleteButton;
+    if(this.props.currentUser === this.props.user){
+      deleteButton = (<a href="#">Delete</a>)
+    }
     return(
-      <li key={this.props.id} data-user={this.props.user}> {this.props.text} </li>
-    
+      <li key={this.props.id} data-user={this.props.user}> {this.props.text} {deleteButton} </li>
+
     )
   }
 }
